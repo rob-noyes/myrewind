460fe17d4feeaf547e4ec977d74fac09a0fe6c9d
Rob Noyes <robertjnoyes1@gmail.com>
2021-12-06 09:34:16 -0800

    trending section fix about iterable object

--- src/App.js
+++ src/App.js
@@ -13,26 +13,8 @@ const theme = createTheme({
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [favorites, setFavorites] = useState([
-    {
-      adult: false,
-      backdrop_path: '/iPnH51khswDrYij6XIBHKlAznW.jpg',
-      genre_ids: [12, 28, 878],
-      id: 11,
-      original_language: 'en',
-      original_title: 'Star Wars',
-      overview:
-        'Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.',
-      popularity: 66.661,
-      poster_path: '/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg',
-      release_date: '1977-05-25',
-      title: 'Star Wars',
-      video: false,
-      vote_average: 8.2,
-      vote_count: 16347,
-    },
-  ]);
-  const [searchValue, setSearchValue] = useState('star wars');
+  const [favorites, setFavorites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   const [trending, setTrending] = useState([]);
 
   // fetch movie API
