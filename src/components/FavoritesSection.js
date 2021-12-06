91ad33cc71bb390357de98bb343507e9778f9052
Rob Noyes <robertjnoyes1@gmail.com>
2021-12-06 09:27:16 -0800

    test update

--- src/components/FavoritesSection.js
+++ src/components/FavoritesSection.js
@@ -25,30 +25,41 @@ const style = {
 };
 
 export default function FavoritesSection({ favorites, removeFavoriteMovie }) {
+  if (favorites === null) {
+    return (
+      <Box>
+        <Typography variant='h3' fontWeight='light' m={1}>
+          Favorites
+        </Typography>
+      </Box>
+    );
+  }
   return (
     <Box>
       <Typography variant='h3' fontWeight='light' m={1}>
         Favorites
       </Typography>
       <Box sx={style.row}>
-        {favorites.map((movie, index) => (
-          <Box className='image-container' key={index}>
-            <Button sx={style.button}>
-              <Link to={`/movie/${movie.title.replace(/[.':\s]/g, '')}`}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-                  alt='movie'
-                />
-              </Link>
-              <Box
-                className='overlay'
-                onClick={() => removeFavoriteMovie(movie)}
-              >
-                <RemoveFavorite />
-              </Box>
-            </Button>
-          </Box>
-        ))}
+        {favorites
+          .filter((movie) => movie.poster_path !== null)
+          .map((movie, index) => (
+            <Box className='image-container' key={index}>
+              <Button sx={style.button}>
+                <Link to={`/movie/${movie.title.replace(/[.':\s]/g, '')}`}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                    alt='movie'
+                  />
+                </Link>
+                <Box
+                  className='overlay'
+                  onClick={() => removeFavoriteMovie(movie)}
+                >
+                  <RemoveFavorite />
+                </Box>
+              </Button>
+            </Box>
+          ))}
       </Box>
     </Box>
   );
