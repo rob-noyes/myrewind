2d61e7beacd41204ca6784545c551b62588be993
Rob Noyes <robertjnoyes1@gmail.com>
2022-01-06 15:47:55 -0800

    updated Accounts Page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Account() {
   return (
     <div className='flex justify-center h-screen'>
-      <h2 className='pt-48 text-3xl text-textPrimary'>
+      <h2 className='pt-48 text-3xl text-textPrimary text-center'>
         Accounts Not yet Implemented :)
       </h2>
     </div>
