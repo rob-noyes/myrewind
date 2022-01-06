7cdbd9e5b9756278cdc5ab55faa0f5e7ae72e318
Rob Noyes <robertjnoyes1@gmail.com>
2022-01-06 15:56:30 -0800

    account update

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -4,7 +4,7 @@ function Account() {
   return (
     <div className='flex justify-center h-screen'>
       <h2 className='pt-48 text-3xl text-textPrimary text-center'>
-        Accounts Not yet Implemented :)
+        Accounts coming soon!
       </h2>
     </div>
   );
